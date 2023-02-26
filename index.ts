e2c2ef56b28cb99c0bfd825e745e05b408825ad2
Muhammad Talha <92953591+talha12121@users.noreply.github.com>
2023-02-26 18:56:36 +0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,47 @@
+                             //   Question no 2
+
+// let personName:string = "Talha";
+// let Message:string = "Hello" +" " + `${personName}` +" " + "would you like to learn some Python today?";
+// console.log(Message);
+
+                            //   Question no 3
+
+// let pName:string = "talha"
+// let upperCase = pName.toUpperCase();
+// let lowerCase  = pName.toLowerCase();
+// function titleCase() {
+//         return pName.split(" ").reduce( (s, c) =>
+//         (c.charAt(0).toUpperCase() + c.slice(1)), '');
+//       }
+
+// console.log(upperCase);
+// console.log(lowerCase);
+// console.log(titleCase());
+
+                      // Question no 4
+                      
+// console.log("Albert Einstein once said,"+ `"A person who never made a mistake never tried anything new"`)
+                      
+                      // Question no 5
+                      
+// let famous_person:string = "Ralph Waldo Emerson " ;
+// let message:string = "Life is a journey, not a destination" ;
+// console.log(`${famous_person}`+ "once said : " + `"${message}"`);
+
+                      // Question no 6
+
+
+// let  name1="\tTalha\t";
+// console.log("Before :"+name1);
+  // console.log("After  :"+name1.trim());
+
+                      //  Question no 7 0r 8
+
+// console.log("Addition = " + `${5+3}`)
+// console.log("Subtraction = " + `${12-4}`)
+// console.log("Multiplication = " + `${4*2}`)
+// console.log("Division = " + `${16/2}`)
+ 
+                      //  Question no 9 
+
+                         
